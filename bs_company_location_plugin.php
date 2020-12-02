45cca5fa41349e050702e5b9dd5a50c394033468
Bryan Siegel <bryansiegel@gmail.com>
2020-12-02 10:45:51 -0800

    Plugin Declaration

--- bs_company_location_plugin.php
+++ bs_company_location_plugin.php
@@ -1,4 +1,15 @@
 <?php 
+/*
+Plugin Name: WordPress Company Location Plugin
+Plugin URI:  https://github.com/bryansiegel/Wordpress-Company-Location-Plugin/
+Description: Creates an interfaces to manage store / business locations on your website. Useful for showing location based information quickly. Includes both a widget and shortcode for ease of use.
+Version:     0.0.1
+Author:      Bryan Siegel
+Author URI:  https://bryansiegel.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
 //block direct access
 defined('ABSPATH') or die('Access Denied');
 
